1de5b9005756ff63a40ce33c195febe365a593b2
Chathuka-Pehesara <CHathuka02Pehesara@gmail.com>
2026-09-18 09:17:50 +0530

    feat: implement forward_chain inference method

--- logic_engine.py
+++ logic_engine.py
@@ -14,3 +14,17 @@ class KnowledgeBase:
     def clear_facts(self):
         """Empty the facts set."""
         self.facts.clear()
+
+    def forward_chain(self):
+        """Deduce new facts using data-driven forward chaining."""
+        new_facts_added = True
+        
+        while new_facts_added:
+            new_facts_added = False
+            
+            for premises, conclusion in self.rules:
+                if conclusion not in self.facts:
+                    # Modus Ponens Check
+                    if all(premise in self.facts for premise in premises):
+                        self.facts.add(conclusion)
+                        new_facts_added = True
